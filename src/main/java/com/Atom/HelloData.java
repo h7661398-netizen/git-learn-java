092f692f082164324872f3a16c51ce335f351776
Atom <h7661398@gmail.com>
2025-12-29 21:01:08 +0800

    Update HelloData.java

--- src/main/java/com/Atom/HelloData.java
+++ src/main/java/com/Atom/HelloData.java
@@ -3,5 +3,6 @@ package com.Atom;
 public class HelloData {
     public static void main(String[] args) {
         System.out.println("Hello Data!");
+        System.out.println("Hello Atom!");
     }
 }
