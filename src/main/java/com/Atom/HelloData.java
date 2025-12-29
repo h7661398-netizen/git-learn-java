a21e2f33d361c5390fd30e474623e1a616c1dcd8
Atom <h7661398@gmail.com>
2025-12-30 01:46:56 +0800

    red black

--- src/main/java/com/Atom/HelloData.java
+++ src/main/java/com/Atom/HelloData.java
@@ -4,5 +4,7 @@ public class HelloData {
     public static void main(String[] args) {
         System.out.println("Hello Data!");
         System.out.println("Hello Atom!");
+        System.out.println("red");
+        System.out.println("black");
     }
 }
