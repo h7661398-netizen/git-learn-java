d0f9f0d41e09c14cae1a5cc19c5e118eeb946b7b
Atom <h7661398@gmail.com>
2025-12-31 17:33:04 +0800

    update

--- src/main/java/com/Atom/HelloData.java
+++ src/main/java/com/Atom/HelloData.java
@@ -4,5 +4,6 @@ public class HelloData {
     public static void main(String[] args) {
         System.out.println("Hello Data!");
         System.out.println("Hello Atom!");
+        System.out.println("Hello Atom!");
     }
 }
